e0bd0dcf91bc9c95e03ec72eabd44254f17b9ae7
Chazona <chznbaum@gmail.com>
2017-06-28 16:41:13 -0400

    Generated updates by scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :updates
   get 'profile', to: 'users#profile'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }

3f122da6f9d815b436ffacff30951cd9c7c9fdb0
Chazona <chznbaum@gmail.com>
2017-06-30 00:04:27 -0400

    Implemented basic update and comment views

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,16 +1,9 @@
 class UpdatesController < ApplicationController
-  before_action :find_kid
   def index
     @updates = Update.all
   end
 
   def show
     @update = Update.find(params[:id])
-  end
-
-  private
-
-  def find_kid
-    @kid = Kid.find_by_id(params[:kid_id])
   end
 end

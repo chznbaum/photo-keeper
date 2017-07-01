2f00efa9722f6d6ff0fa9a01a01d46373e8bd647
Chazona <chznbaum@gmail.com>
2017-07-01 17:52:49 -0400

    Reseeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,131 @@ User.create!(
 end
 
 puts "#{User.count} users created."
+
+Site.create!(
+  name: 'Sample Site',
+  subdomain: 'sample-site',
+  user_id: 1,
+)
+
+Site.create!(
+  name: 'Example Site',
+  subdomain: 'example-site',
+  user_id: 1,
+)
+
+ActsAsTenant.current_tenant = Site.find_by(subdomain: 'sample-site')
+
+3.times do
+  Kid.create!(
+    name: Faker::Name.name,
+    age: rand(1..8),
+    photo: 'http://placehold.it/150x150',
+  )
+end
+
+puts "#{Kid.count} kids created."
+
+10.times do
+  wordcount = rand(8..30)
+  Update.create!(
+    upload: 'http://placehold.it/800x600',
+    content: Faker::Lorem.words(wordcount).join(" "),
+    kid_id: rand(1..Kid.count),
+  )
+end
+
+puts "#{Update.count} updates created."
+
+15.times do
+  Member.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'passwordpassword',
+    avatar: 'http://placehold.it/150x150',
+  )
+end
+
+puts "#{Member.count} members created."
+
+30.times do
+  wordcount = rand(5..25)
+  Comment.create!(
+    content: Faker::Lorem.words(wordcount).join(" "),
+    commentable_id: rand(1..10),
+    commentable_type: 'Update',
+    member_id: rand(1..Member.count),
+  )
+end
+
+puts "#{Comment.count} update comments created."
+
+100.times do
+  wordcount = rand(5..25)
+  Comment.create!(
+    content: Faker::Lorem.words(wordcount).join(" "),
+    commentable_id: rand(11..Comment.count),
+    commentable_type: 'Comment',
+    member_id: rand(1..Member.count),
+  )
+end
+
+puts "#{Comment.count} update and comment comments created."
+
+ActsAsTenant.current_tenant = Site.find_by(subdomain: 'example-site')
+
+3.times do
+  Kid.create!(
+    name: Faker::Name.name,
+    age: rand(1..8),
+    photo: 'http://placehold.it/150x150',
+  )
+end
+
+puts "#{Kid.count} kids created."
+
+10.times do
+  wordcount = rand(8..30)
+  Update.create!(
+    upload: 'http://placehold.it/800x600',
+    content: Faker::Lorem.words(wordcount).join(" "),
+    kid_id: rand(1..Kid.count),
+  )
+end
+
+puts "#{Update.count} updates created."
+
+15.times do
+  Member.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'passwordpassword',
+    avatar: 'http://placehold.it/150x150',
+  )
+end
+
+puts "#{Member.count} members created."
+
+30.times do
+  wordcount = rand(5..25)
+  Comment.create!(
+    content: Faker::Lorem.words(wordcount).join(" "),
+    commentable_id: rand(11..20),
+    commentable_type: 'Update',
+    member_id: rand(16..30),
+  )
+end
+
+puts "#{Comment.count} update comments created."
+
+100.times do
+  wordcount = rand(5..25)
+  Comment.create!(
+    content: Faker::Lorem.words(wordcount).join(" "),
+    commentable_id: rand(131..(130 + Comment.count)),
+    commentable_type: 'Comment',
+    member_id: rand(16..30),
+  )
+end
+
+puts "#{Comment.count} update and comment comments created."

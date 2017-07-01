984c527812a3d7467479da8895adb360ef2a86af
Chazona <chznbaum@gmail.com>
2017-07-01 16:30:02 -0400

    Added members model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701201918) do
+ActiveRecord::Schema.define(version: 20170701202836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,27 @@ ActiveRecord::Schema.define(version: 20170701201918) do
     t.datetime "updated_at", null: false
     t.index ["site_id"], name: "index_likes_on_site_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "name"
+    t.text "avatar"
+    t.bigint "site_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+    t.index ["site_id"], name: "index_members_on_site_id"
   end
 
   create_table "sites", force: :cascade do |t|

d02507c473af32b6fe20afb19d4a86ec8bad512a
Chazona <chznbaum@gmail.com>
2017-07-02 15:47:32 -0400

    Updated kid views

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,13 +1,17 @@
 class KidsController < ApplicationController
   before_action :set_kid, only: [:show]
     
-  layout 'update'
+  layout 'album'
 
   def index
     @kids = Kid.all
+    @page_title = 'Kids'
+    @page_description = 'All the kids under this site.'
   end
 
   def show
+    @page_title = @kid.name
+    @page_description = "The updates involving #{@kid.name}."
   end
 
   private
